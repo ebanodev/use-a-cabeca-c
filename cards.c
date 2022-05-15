921f0469ec3ef46fb9237fd9c990bff7a468c9ba
Ébano Assumpção <ebanoassumpcao@gmail.com>
2022-05-15 19:18:01 -0300

    página 38

--- cards.c
+++ cards.c
@@ -4,25 +4,35 @@
 int main ()
 {
 	char card_name[3];
+	while ( card_name[0] != 'X') 
+	{
 	puts("Enter your cardname: ");
 	scanf("%2s", card_name);
 	int val = 0;
-	switch (card_name[0]){
-	case 'K':
-	case 'Q':
-	case 'J':
+	switch (card_name[0])
+		{
+		case 'K':
+		case 'Q':
+		case 'J':
 		val = 10;
-	break;
-	case 'A':
+		break;
+		case 'A':
 		val = 11;
-	break;
-	default:
-	val = atoi(card_name);
-	}	
+		break;
+		case 'X':
+		continue;
+		default:
+		val = atoi(card_name);
+		if ((val < 1) || (val > 10))
+			{
+			puts ("I dont undestand the value!");
+			continue;
+			}
+		}	
 	if ( (val > 2) && (val < 7))
 	puts ("count has gone up");
-	
 	else if (val == 10)
 	puts ("count has gone down");
+	}
 	return 0;
 }
